8489677bfe77f5638ae1ef3b63f6ad45d4cb4138
Amosawy <544651826@qq.com>
2019-10-16 21:05:03 +0800

    Amos

--- reptile/dynamic_content/test.py
+++ reptile/dynamic_content/test.py
@@ -1,6 +1,7 @@
-<<<<<<< HEAD
+
 import urllib.request
 import urllib.parse
+
 LOGIN_URL='http://example.webscraping.com/usr/login'
 LOGIN_EMAIL='amosawy123@163.com'
 LOGIN_PASSWORD='ai794613'
@@ -13,19 +14,5 @@ print(response.geturl())
 print(response.code)
 
 print(response.read().decode('utf-8'))
-=======
 
 
-# from PySide
-
-import json
-
-from simple.MongoDB import MongoCache
-from simple.downloader import Downloader
-
-cache=MongoCache()
-D=Downloader(cache=cache)
-url='http://example.webscraping.com/places/ajax/search.json?&search_term=.&page_size=500&page=0'
-a=json.loads(D(url).decode('utf-8'))['num_pages']
-print(a)
->>>>>>> 6a7bf5f2de02742b0b45a14f5d8eec3b102b176d
